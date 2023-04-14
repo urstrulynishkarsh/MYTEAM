3ff9db4acc8f0680c2d1bc6a7d519347431ed61e
urstrulynishkarsh <206301010@gkv.ac.in>
2023-04-14 19:47:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Testimonials from "./components/Testimonials";
 
 const App = () => {
   return (
-    <div className="w-[100vw] h-[100vh] max-[500px]:absolute top-[-5.5rem]  flex flex-col justify-center items-center gap-y-5 bg-gray-200 ">
+    <div className="w-[100vw] h-[100vh]  flex flex-col justify-center items-center gap-y-5 bg-gray-200 ">
       <div className="text-center">
-        <h1 className="text-4xl max-[500px]:mt-[5rem] max-[500px]:absolute max-[500px]:left-[17%] max-[500px]:top-[5%]   font-bold">Our Testimonials</h1>
+        <h1 className="text-4xl font-bold">Our Testimonials</h1>
         {/* <div className="bg-violet-500   w-1/5 mt-1 mx-auto"></div> */}
         <div className="w-[9rem] h-[4px] bg-violet-500 mt-[10px] md:mt-[10px] mx-auto"></div>
         <Testimonials reviews={reviews} />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,10 +4,10 @@ import {FaQuoteLeft,FaQuoteRight } from "react-icons/fa";
 const Cards = (props) => {
     let review=props.review;
   return (
-    <div className='flex flex-col md:relative'>
-        <div className='absolute max-[500px]:top-[11rem] md:top-[-6rem]  z-[10] mx-auto' >
-            <img className="aspect-square  object-cover rounded-full md:w-[140px] md:h-[140px] w-[100px] h-[100px] z-[25]" src={review.image}/>
-            <div className='absolute   top-[0rem] z-[-20] mx-auto rounded-full md:w-[140px] md:h-[140px] w-[100px] h-[100px] left-[7px] bg-red-400'></div>
+    <div className='flex flex-col relative'>
+        <div className='absolute top-[-7rem]  z-[10] mx-auto' >
+            <img className="aspect-square  object-cover rounded-full  w-[140px] h-[140px] z-[25]" src={review.image}/>
+            <div className='absolute   top-[0rem] z-[-20] mx-auto rounded-full w-[140px] h-[140px] left-[7px] bg-red-400'></div>
         </div>
         <div className='text-center mt-7'>
             <p className='tracking-wider font-bold text-2xl'> {review.name}</p>
